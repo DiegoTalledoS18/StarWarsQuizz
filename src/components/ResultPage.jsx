ac4b68602742e0fe3eb752ec8a9ed75de75632f3
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2024-01-31 17:44:32 -0500

    getting final score with total of questions

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -2,10 +2,11 @@ import { useQuestionStore } from "../store/questions.js";
 
 export default function ResultPage() {
     const finalScore = useQuestionStore.getState().getFinalScore(useQuestionStore.getState());
+    const totalOfQuestions = useQuestionStore.getState().questions.length;
 
     return (
         <>
-            <p>{finalScore}</p>
+            <p>{finalScore}/{totalOfQuestions}</p>
         </>
     );
 }

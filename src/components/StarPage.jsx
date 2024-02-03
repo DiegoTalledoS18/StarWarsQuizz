607beb306666b7ee63cb60bb739ae95a222334c4
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2024-02-02 20:04:11 -0500

    adding blur

--- src/components/StarPage.jsx
+++ src/components/StarPage.jsx
@@ -2,8 +2,9 @@ import {Box, Button, styled, Grid,Typography} from "@mui/material";
 import {NavLink} from "react-router-dom";
 import "../styles/StarPage.css"
 import SWLogo from "../assets/SW_LOGO_BLUR.png";
+import SD_L from "../assets/sd_1.png";
+import SD_R from "../assets/sd_2.png";
 import StarsBackground from "../assets/stars.png";
-import SpaceBackground from "../assets/space.png";
 import { makeStyles } from "@material-ui/core/styles";
 import {MouseParallaxChild, MouseParallaxContainer} from "react-parallax-mouse";
 import {useEffect, useState} from "react";
@@ -11,40 +12,61 @@ import {useEffect, useState} from "react";
 const useStyles = makeStyles({
     root: {
         width: "100%",
-        height: "100vh",
-        backgroundColor: "#000000"
-    }
+        minHeight: "100vh",
+        backgroundColor: "#000000",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
 
 export default function StarPage(){
     const classes = useStyles();
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const handleMouseMove = (event) => {
         setMousePosition({ x: event.clientX, y: event.clientY });
     };
 
     useEffect(() => {
-        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener("mousemove", handleMouseMove);
         return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener("mousemove", handleMouseMove);
         };
     }, []);
 
+    const calculateBackgroundBlur = (xPosition, windowWidth) => {
+        const distanceFromCenter = Math.abs(xPosition - windowWidth / 2);
+        const maxBlur = 0.6;
+        const blurFactor = 1 - Math.min(distanceFromCenter / (windowWidth / 2), 1);
+        const blurValue = blurFactor * maxBlur;
+        return `${blurValue}px`;
+    };
+
     const calculateBlur = (yPosition, windowHeight) => {
-        // Calculamos la distancia vertical desde el centro de la ventana
         const distanceFromCenter = Math.abs(yPosition - windowHeight / 2);
-        // Definimos un valor máximo de desenfoque
-        const maxBlur = 1;
-        // Calculamos el factor de desenfoque basado en la distancia
+        const maxBlur = 1.5;
         const blurFactor = Math.min(distanceFromCenter / (windowHeight / 2), 1);
-        // Calculamos el desenfoque en función del factor y el máximo
+        const blurValue = blurFactor * maxBlur;
+        return `${blurValue}px`;
+    };
+    const calculateSDLBlur = (yPosition, windowHeight) => {
+        const distanceFromCenter = Math.abs(yPosition - windowHeight / 2);
+        const maxBlur = 1.5;
+        const blurFactor = Math.min(distanceFromCenter / (windowHeight / 2), 1);
+        const blurValue = blurFactor * maxBlur;
+        return `${blurValue}px`;
+    };
+    const calculateSDRBlur = (yPosition, windowHeight) => {
+        const distanceFromCenter = Math.abs(yPosition - windowHeight / 2);
+        const maxBlur = 1.5;
+        const blurFactor = Math.min(distanceFromCenter / (windowHeight / 2), 1);
         const blurValue = blurFactor * maxBlur;
         return `${blurValue}px`;
     };
 
     const Img= styled("img")({
-        width: 550,
     })
 
     return (
@@ -54,14 +76,16 @@ export default function StarPage(){
                     className="parallax"
                     containerStyle={{
                         width: "100%",
-                        height: "100%"
+                        height: "100%",
+                        display: "grid",
+                        gridTemplateColumns: "1fr 1fr 1fr", // Define tres columnas de igual tamaño
                     }}
                     globalFactorX={0.3}
                     globalFactorY={0.3}
                     resetOnLeave
                 >
                     <MouseParallaxChild
-                        factorX={0.1}
+                        factorX={0.05}
                         factorY={0.1}
                         style={{
                             backgroundImage: `url(${StarsBackground})`,
@@ -70,12 +94,27 @@ export default function StarPage(){
                             position: "absolute",
                             width: "100%",
                             height: "100%",
-                            backfaceVisibility: "hidden"
+                            backfaceVisibility: "hidden",
+                            filter: `blur(${calculateBackgroundBlur(mousePosition.x, window.innerWidth)})` // Aplica el filtro de desenfoque dinámico
                         }}
                     />
                     <MouseParallaxChild
-                        factorX={-0.022}
-                        factorY={-0.022}
+                        factorX={-0.02}
+                        factorY={0.08}
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            height: "100%",
+                            filter: `blur(${calculateSDLBlur(mousePosition.x, window.innerWidth)})`
+
+                        }}
+                    >
+                        <Img src={SD_L} style={{width:"100px"}} alt="Star Wars Logo"></Img>
+                    </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={-0.01}
+                        factorY={-0.01}
                         style={{
                             display: "flex",
                             alignItems: "center",
@@ -93,12 +132,25 @@ export default function StarPage(){
                         }}>
                             <img src={SWLogo} alt="Star Wars Logo"></img>
                             <Typography variant="h4" component="h1" sx={{ color: "#ffffff", mt: 1 }}>MAY THE FORCE BE WITH YOU</Typography>
-                            <Typography variant="p" component="h3" sx={{ color: "#FFFFFF", mt: 1 }}>BY DIEGO TALLEDO S.</Typography>
+                            <Typography variant="h5" component="h3" sx={{ color: "#FFFFFF", mt: 1 }}>By Diego Talledo S.</Typography>
                             <Button variant="outlined" sx={{ color: '#ffec00', border: '1px solid #ffec00', mt: 2 }}
                                     component={NavLink}
                                     to="/question/1">EMPEZAR</Button>
                         </Box>
                     </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.09}
+                        factorY={-0.005}
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            height: "100%",
+                            filter: `blur(${calculateSDRBlur(mousePosition.x, window.innerWidth)})`
+
+                        }}
+                    >
+                        <Img src={SD_R} style={{width:"300px"}} alt="Star Wars Logo"></Img></MouseParallaxChild>
                 </MouseParallaxContainer>
             </Grid>
 
